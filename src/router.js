53a46a91e606349cc840c660a418a994dfe8d1f6
PetSD <1913055941@qq.com>
2022-05-28 09:39:13 +0800

    路由封装

--- src/router.js
+++ src/router.js
@@ -1,5 +1,4 @@
-
-import Router from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from './pages/home.vue'
 import Index from './pages/index.vue'
 import Product from './pages/product.vue'
@@ -11,56 +10,61 @@ import OrderList from './pages/orderList.vue'
 import OrderPay from './pages/orderPay.vue'
 
 
+const routes = [
+    {
+        path:'/',
+        name:'home',
+        component:Home,
+        children:[
+            {
+                path:'/index',
+                name:'index',
+                component:Index
+            },
+            {
+                path:'/product/:id',
+                name:'product',
+                component:Product
+            },
+            {
+                path:'/detail',
+                name:'detail',
+                component:Detail
+            },
+        ]
+    },{
+        path:'/cart',
+        name:'cart',
+        component:Cart,
+    },{
+        path:'/order',
+        name:'order',
+        component:Order,
+        children:[
+            {
+                path:'/list',
+                name:'order-list',
+                component:OrderList,
+            },
+            {
+                path:'/list',
+                name:'order-comfirm',
+                component:OrderConfirm,
+            },
+            {
+                path:'/pay',
+                name:'order-pay',
+                component:OrderPay,
+            },
+        ]
+    }
+]
 
 
-export default new Router({
-    routes:[
-        {
-            path:'/',
-            name:'home',
-            component:Home,
-            children:[
-                {
-                    path:'/index',
-                    name:'index',
-                    component:Index
-                },
-                {
-                    path:'/product/:id',
-                    name:'product',
-                    component:Product
-                },
-                {
-                    path:'/detail',
-                    name:'detail',
-                    component:Detail
-                },
-            ]
-        },{
-            path:'/cart',
-            name:'cart',
-            component:Cart,
-        },{
-            path:'/order',
-            name:'order',
-            component:Order,
-            children:[
-                {
-                    path:'/list',
-                    name:'order-list',
-                    component:OrderList,
-                },
-                {
-                    path:'/list',
-                    name:'order-comfirm',
-                    component:OrderConfirm,
-                },
-                {
-                    path:'/pay',
-                    name:'order-pay',
-                    component:OrderPay,
-                },
-            ]
-        }
-    ]
-});
+
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+export default router
