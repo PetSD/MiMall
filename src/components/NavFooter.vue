90f0229872a9a827f282c054c10ddf0c6d13239e
PetSD <1913055941@qq.com>
2022-06-07 18:21:57 +0800

    Navfooter编写

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,6 +1,18 @@
 <template>
-    <div>
-
+    <div class="footer">
+        <div class="footer-logo">
+            <img src="/imgs/logo-footer.png" alt="">
+            <p>小米商城</p>
+        </div>
+        <div class="footer-link">
+            <a href="" target="_blank">个人主页</a><span>|</span>
+            <a href="" target="_blank">vue全栈课程</a><span>|</span>
+            <a href="" target="_blank">React全家桶</a><span>|</span>
+            <a href="" target="_blank">微信支付转向课程</a>
+        </div>
+        <div class="copyright">
+             Copyright ©2019 <span class="domain">mi.futurefe.com</span> All Rights Reserved.
+        </div>
     </div>
 </template>
 
@@ -11,3 +23,38 @@ export default{
     
 }
 </script>
+
+<style lang="scss">
+    .footer{
+        height: 234px;
+        border-top: 4px solid #ff6600;
+        background-color: #333333;
+        color: #999999;
+        font-size: 16px;
+        text-align: center;
+        .footer-logo{
+            margin-top: 46px;
+            margin-bottom: 31px;
+            img{
+                width: 53px;
+                height: 36px;
+                margin-bottom: 13px;
+            }
+        }
+        .footer-link{
+            a{
+                color: #999999;
+                display: inline-block;
+            }
+            span{
+                margin: 0 10px;
+            }
+            margin-top: 13px;
+        }
+        .copyright{
+            .domain{
+                color: #ff6600;
+            }
+        }
+    }
+</style>
